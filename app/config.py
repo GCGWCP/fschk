bc36bf3159ce7b2730712bfe4abe1943c32bc116
knotech <gabe.schroder@gmail.com>
2017-02-11 16:42:17 -0600

    Started adding models for database

--- app/config.py
+++ app/config.py
@@ -1,30 +1,31 @@
-import os
-
-
-class Environment(env):
+class Config():
     def __init__(self, env):
-        self.app_env = {
-            'Dev': {
-                'ABS_PATH_TO_APP': os.getcwd(),
-                'POSTGRESURL': '',
-                'POSTGRESPWORD': '',
-                'MONGOURL': '',
-                'MONGOPWORD': ''
-            },
-            'Stage': {
-                'POSTGRESURL': '',
-                'POSTGRESPWORD': '',
-                'MONGOURL': '',
-                'MONGOPWORD': ''
-            },
-            'Prod': {
-                'POSTGRESURL': '',
-                'POSTGRESPWORD': '',
+        if env is 'Dev':
+            self.app_env = {
+                'POSTGRES_URL': 'postgresql://schroder@localhost:5432/',
+                'POSTGRES_PWORD': 'change this shit',
+                'POSTGRES_DB': 'fschk',
                 'MONGOURL': '',
                 'MONGOPWORD': ''
             }
-        }
+        elif env is 'Stage':
+            self.app_env = {
+                'POSTGRESURL': '',
+                'POSTGRESPWORD': '',
+                'POSTGRES_DB': 'fschk',
+                'MONGOURL': '',
+                'MONGOPWORD': ''
+            }
+        elif env is 'Prod':
+            self.app_env = {
+                'POSTGRESURL': '',
+                'POSTGRESPWORD': '',
+                'POSTGRES_DB': 'fschk',
+                'MONGOURL': '',
+                'MONGOPWORD': ''
+            }
 
+        return self
 
 
 if __name__ == '__main__':
--- db/dbs.py
+++ db/dbs.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship, backref
+Base = declarative_base()
+engine = create_engine('postgresql://schroder@localhost:5432/fschk')
 
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
 
+class File(Base):
+    __tablename__ = 'Files'
 
-def create_db():
-    pass
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String, nullable=True)
+    root_path = Column(String, nullable=True)
+    size = Column(Integer, nullable=True)
+    permissions = Column(Integer, nullable=True)
+    created = Column(DateTime, nullable=True)
+    last_modified = Column(DateTime, nullable=True)
+    last_accessed = Column(DateTime, nullable=True)
+    owner = Column(Integer, nullable=True)
+    group = Column(Integer, nullable=True)
+    inode = Column(Integer, nullable=True)
+    file_type = Column(String, nullable=True)
+    ext_attr = Column(String, nullable=True)
+    sticky_bit = Column(Integer, nullable=True)
+    encoding = Column(String, nullable=True)
+    sha256 = Column(String, nullable=True)
+    sha512 = Column(String, nullable=True)
+
+def create_table():
+    
 
 
 def init_tables():
     pass
 
 
-def connect_to_pgsql():
-    pass
+
