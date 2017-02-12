a59c72b98ba672d2a957717ed23c6c749f60bfa5
knotech <gabe.schroder@gmail.com>
2017-02-11 23:37:29 -0600

    Got a pseudo proper config file going. Began writing db middleware.

--- db/dbs.py
+++ db/dbs.py
@@ -1,42 +1,73 @@
 #!/usr/bin/env python
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from models import File
+from config import Config
 Base = declarative_base()
-engine = create_engine('postgresql://schroder@localhost:5432/fschk')
+
+app = Config()
+app.set_env('dev')
 
 
-class File(Base):
-    __tablename__ = 'Files'
-
-    id = Column(Integer, primary_key=True)
-    file_name = Column(String, nullable=True)
-    root_path = Column(String, nullable=True)
-    size = Column(Integer, nullable=True)
-    permissions = Column(Integer, nullable=True)
-    created = Column(DateTime, nullable=True)
-    last_modified = Column(DateTime, nullable=True)
-    last_accessed = Column(DateTime, nullable=True)
-    owner = Column(Integer, nullable=True)
-    group = Column(Integer, nullable=True)
-    inode = Column(Integer, nullable=True)
-    file_type = Column(String, nullable=True)
-    ext_attr = Column(String, nullable=True)
-    sticky_bit = Column(Integer, nullable=True)
-    encoding = Column(String, nullable=True)
-    sha256 = Column(String, nullable=True)
-    sha512 = Column(String, nullable=True)
-
-    def __repr__(self):
-        return "<File (file_name='%s', root_path='%s', size='%d', permissions='%d', created='%s', last_modified='%s', last_accessed='%s', owner='%d', group='%d', inode='%d', file_type='%s', ext_attr='%s', sticky_bit='%s', encoding='%s', sha256='%s', sha512='%s')>" % (self.file_name, self.root_path, self.size, self.permissions, self.created, self.last_modified, self.owner, self.group, self.inode, self.created, self.last_modified, self.last_accessed, self.file_type, self.ext_attr, self.sticky_bit, self.encoding, self.sha256, self.sha512)
-
-def create_table():
-    
+def connect(user, password, db, host='localhost', port=5432):
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url.format(user, password, host, port, db)
+    con = create_engine(url, client_encoding='utf8')
+    meta = MetaData(bind=con, reflect=True)
+    return con, meta
 
 
-def init_tables():
-    pass
+def select(table, column=[], value=[]):
+    con, meta = connect(
+        app.conf['POSTGRES_USER'],
+        app.conf['POSTGRES_PWORD'],
+        app.conf['POSTGRES_DB']
+    )
+    if not column:
+        s = con.select([table])
+        result = s.execute()
+    elif column and not value:
+        s = con.select([table.col for col in column])
+        result = s.execute()
+    elif column and value:
+        s = con.select([table]).where(table.column == value)
+        result = s.execute()
+
+    con.close()
+    return result
 
 
+def insert_row(table, columns={}):
+    con, meta = connect(
+        app.conf['POSTGRES_USER'],
+        app.conf['POSTGRES_PWORD'],
+        app.conf['POSTGRES_DB']
+    )
+    cols = [columns]
+    con.execute(meta.tables[table].insert(), cols)
+    con.close()
+
+
+def insert_rows(table, columns={}):
+    con, meta = connect(
+        app.conf['POSTGRES_USER'],
+        app.conf['POSTGRES_PWORD'],
+        app.conf['POSTGRES_DB']
+    )
+    cols = [columns]
+    for col in cols:
+        con.execute(meta.tables[table].insert(), col)
+    con.close()
+
+
+def update_row(table, columns={}):
+    con, meta = connect(
+        app.conf['POSTGRES_USER'],
+        app.conf['POSTGRES_PWORD'],
+        app.conf['POSTGRES_DB']
+    )
+    # select -> insert
+    con.close()
 
